47289e3a971fd5a0f7c424e2fe333cd3a327d557
HelderNunes <haanunes@gmail.com>
2023-07-16 14:24:35 -0300

    Services

---
+++ src/main/java/br/com/banco/services/ContaService.java
@@ -0,0 +1,32 @@
+package br.com.banco.services;
+
+import br.com.banco.entities.Conta;
+import br.com.banco.repositories.ContaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ContaService {
+
+    private final ContaRepository contaRepository;
+
+    @Autowired
+    public ContaService(ContaRepository contaRepository) {
+        this.contaRepository = contaRepository;
+    }
+
+    public Conta save(Conta conta) {
+        return contaRepository.save(conta);
+    }
+
+    public Conta findById(Long id) {
+        return contaRepository.findById(id).orElse(null);
+    }
+
+    public List<Conta> findAll() {
+        return contaRepository.findAll();
+    }
+
+}
---
+++ src/main/java/br/com/banco/services/TransferenciaService.java
@@ -0,0 +1,32 @@
+package br.com.banco.services;
+
+import br.com.banco.entities.Transferencia;
+import br.com.banco.repositories.TransferenciaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TransferenciaService {
+
+    private final TransferenciaRepository transferenciaRepository;
+
+    @Autowired
+    public TransferenciaService(TransferenciaRepository transferenciaRepository) {
+        this.transferenciaRepository = transferenciaRepository;
+    }
+
+    public Transferencia save(Transferencia transferencia) {
+        return transferenciaRepository.save(transferencia);
+    }
+
+    public Transferencia findById(Long id) {
+        return transferenciaRepository.findById(id).orElse(null);
+    }
+
+    public List<Transferencia> findAll() {
+        return transferenciaRepository.findAll();
+    }
+
+}
