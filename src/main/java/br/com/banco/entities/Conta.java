1a19a7682388abbe3c15d9e7ccdfa88ace311030
HelderNunes <haanunes@gmail.com>
2023-07-16 14:08:22 -0300

    Entities

---
+++ src/main/java/br/com/banco/entities/Conta.java
@@ -0,0 +1,27 @@
+package br.com.banco.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+
+/**
+ *
+ * @author Hélder
+ */
+@Data
+@Entity
+@Table(name = "conta")
+public class Conta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_conta")
+    private Long idConta;
+
+    @Column(name = "nome_responsavel")
+    private String nomeResponsavel;
+}
---
+++ src/main/java/br/com/banco/entities/Transferencia.java
@@ -0,0 +1,46 @@
+package br.com.banco.entities;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.Data;
+/**
+ *
+ * @author Hélder
+ */
+
+
+@Data
+@Entity
+@Table(name = "transferencia")
+public class Transferencia {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name = "data_transferencia")
+    private LocalDateTime dataTransferencia;
+    
+    @Column(name = "valor")
+    private BigDecimal valor;
+    
+    @Column(name = "tipo")
+    private String tipo;
+    
+    @Column(name = "nome_operador_transacao")
+    private String nomeOperadorTransacao;
+    
+    @ManyToOne
+    @JoinColumn(name = "conta_id", referencedColumnName = "id_conta")
+    private Conta conta;
+    
+    // getters e setters
+}
